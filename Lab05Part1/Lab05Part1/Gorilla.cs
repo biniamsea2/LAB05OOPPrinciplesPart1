ab1fa88ac84e466ac36b28143db14e352d532bf8
Biniam <biniamt23@yahoo.com>
2019-10-21 12:43:31 -0700

    added classes and objects, worked on digital drawing

---
+++ Lab05Part1/Lab05Part1/Gorilla.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab05Part1
+{
+    class Gorilla : Mammal
+    {
+        public bool BlackColor { get; set; } = true;
+    }
+}
